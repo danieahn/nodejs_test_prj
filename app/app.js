cef0ef806c15a0bcc67ebf2873993838ca504f5c
danieahn <danieahn@gmail.com>
2022-07-03 11:38:54 +0900

    optimize project foler

--- app/app.js
+++ app/app.js
@@ -6,11 +6,11 @@ const app = express();
 const PORT = 3000;
 
 // set view
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 // set routing
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 app.use("/", home);
 
 module.exports = app;
